dc375548c85a56e40f9d7c47c858e1280b043450
Sudo <s86295509@gmail.com>
2025-06-20 21:23:34 -0700

    Add files via upload

---
+++ AppWithCustomizationVar.py
@@ -0,0 +1,17 @@
+import MainFunctions
+#This Page Will Be The Simplest I Can Make For You To Customize The App
+
+#Replace these with what you want
+
+# Replace these URLs with the ones you want to open
+Url_To_Open1 = "https://github.com/SudoBlank/Python-AddWare-for-Windows-Beta-" 
+Url_To_Open2 = "https://github.com/SudoBlank/Python-AddWare-for-Windows-Beta-"
+Url_To_Open3 = "https://github.com/SudoBlank/Python-AddWare-for-Windows-Beta-"
+Url_To_Open4 = "https://github.com/SudoBlank/Python-AddWare-for-Windows-Beta-"
+Url_To_Open5 = "https://github.com/SudoBlank/Python-AddWare-for-Windows-Beta-"
+
+#Time interval in seconds for opening the URLs
+interval = 1
+
+# Call the function to open the App !!!Do not remove!!!
+MainFunctions.OpenStartGui(Url_To_Open1, Url_To_Open2, Url_To_Open3, Url_To_Open4, Url_To_Open5, interval)  # Call the function to start the App with the URL you provided
